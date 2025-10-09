a35c307e51299945553fa4e02ae84eb5eb64f774
junlu.xie <17717304663@163.com>
2025-10-09 19:22:07 +0800

    solidity 任务一

--- solidity-basic/task1/code1_Vote.sol
+++ solidity-basic/task1/code1_Vote.sol
@@ -11,7 +11,7 @@ contract Vote{
     //一个vote函数，允许用户投票给某个候选人
     function vote(address addr) external {
         voters[addr]++;
-        // addrrs.push(addr);
+        addrrs.push(addr);
     }
     //一个getVotes函数，返回某个候选人的得票数
     function getVotes(address addr) external view returns(uint){
@@ -24,16 +24,4 @@ contract Vote{
         }
         delete addrrs;
     }
-
-    //反转字符串 (Reverse String)
-    function reverse(string memory str0) external pure returns(string memory){
-        bytes memory chars = bytes(str0);
-        uint  len = chars.length;
-        bytes memory reserChars = new bytes(len);
-        for(uint i=0;i<len;i++){
-            reserChars[len-i-1]=chars[i];
-        }
-        return string(reserChars);
-    }
-
 }
